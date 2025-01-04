329255f9cf681bbac1d5022b3daa80bf17bc8e18
Vijaya Lingagiri <vlingagiri@gmail.com>
2025-01-04 20:23:03 +0000

    fix bars

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HeroVideo from "./components/HeroVideo"; // Updated import
+import HeroVideo from "./components/HeroVideo";
 import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
 import About from "./components/About";
 import TopBar from "./components/TopBar";
+import BottomBar from "./components/BottomBar";  // Import the BottomBar component
 
 const App = () => (
   <>
     <TopBar />
     <Header />
     <main id="main-content">
-      <HeroVideo /> {/* Replaced HeroSection with HeroVideo */}
-      <About /> 
+      <HeroVideo />
+      <About />
       <Services />
       <Testimonials />
     </main>
     <Footer />
-    <div className="bottom-bar">
-      <p>Call us now to book your event!</p>
-    </div>
+    <BottomBar />  {/* Add the BottomBar here */}
   </>
 );
 
 export default App;
-
-
---
+++ src/components/BottomBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./BottomBar.css";  // Importing the BottomBar.css
+
+const BottomBar = () => {
+  return (
+    <div className="bottom-bar">
+      <p>Call us now to book your event!</p>
+    </div>
+  );
+};
+
+export default BottomBar;
