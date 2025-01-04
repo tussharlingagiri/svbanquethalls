e15a3bc6d39c82e2a6f1080eece8615b64352b67
Vijaya Lingagiri <vlingagiri@gmail.com>
2025-01-04 20:59:40 +0000

    refactor

--- src/components/StickyTest.js
+++ src/components/StickyTest.js
@@ -2,22 +2,26 @@
 import React from "react";
 
 const StickyTest = () => {
-  return (
-    <div
-      style={{
-        position: "fixed",
-        bottom: "20px",
-        right: "20px",
-        padding: "10px",
-        backgroundColor: "#34b7f1",
-        color: "#fff",
-        borderRadius: "50%",
-        zIndex: 9999,
-      }}
-    >
-      Test Button
-    </div>
-  );
-};
+    return null; // Simply return null to not render anything
+  };
+
+// const StickyTest = () => {
+//   return (
+//     <div
+//       style={{
+//         position: "fixed",
+//         bottom: "20px",
+//         right: "20px",
+//         padding: "10px",
+//         backgroundColor: "#34b7f1",
+//         color: "#fff",
+//         borderRadius: "50%",
+//         zIndex: 9999,
+//       }}
+//     >
+//       Test Button
+//     </div>
+//   );
+// };
 
 export default StickyTest;
