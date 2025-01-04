22863f8731244564324b462b265311e51cb8bc39
Vijaya Lingagiri <vlingagiri@gmail.com>
2025-01-04 17:31:26 +0000

    remove about

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HeroVideo from "./components/HeroVideo"; // Updated import
-import About from "./components/About";
 import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
 
