595d77b5052618c7c2b5009ae8818a3de17d2ae6
Vijaya Lingagiri <vlingagiri@gmail.com>
2025-01-04 17:35:53 +0000

    fix blank page

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';  // Import HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Wrap your App component in HashRouter */}
+    <HashRouter basename={process.env.PUBLIC_URL}> 
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
