cfbc689dd81265fa37bd5a9527cf18cc530fb6cf
Vijaya Lingagiri <vlingagiri@gmail.com>
2025-01-04 18:49:46 +0000

    revert

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,35 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./TopBar.css";
 
 const TopBar = () => {
-  const [hidden, setHidden] = useState(false);
-
-  useEffect(() => {
-    let lastScrollTop = 0;
-    const onScroll = () => {
-      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      if (scrollTop > lastScrollTop) {
-        setHidden(true); // Hide top bar when scrolling down
-      } else {
-        setHidden(false); // Show top bar when scrolling up
-      }
-      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
-    };
-
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  }, []);
-
   return (
-    <div className={`top-bar ${hidden ? "hidden" : ""}`}>
+    <div className="top-bar">
       <div className="logo">
         <a href="/">
-          <img
-            src={`${process.env.PUBLIC_URL}/logo.png`}
-            alt="SV Banquet Halls"
-          />
+        <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="SV Banquet Halls" />
+
         </a>
       </div>
       <nav className="nav-menu">
