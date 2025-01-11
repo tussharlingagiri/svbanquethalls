29998714304ed838e0f39bd0ad0f879d087e32d0
Vijaya Lingagiri <vlingagiri@gmail.com>
2025-01-11 16:01:08 +0000

    sticy footer for mobile

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { FaPhoneAlt, FaWhatsapp } from "react-icons/fa"; // Importing icons
-import "./BottomBar.css"; // Importing the CSS
+import { FaPhoneAlt, FaWhatsapp } from "react-icons/fa";
+import "./BottomBar.css";
 
 const BottomBar = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-  // Update `isMobile` state on window resize
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
@@ -15,22 +14,27 @@ const BottomBar = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  // Only render the bottom bar if the screen size is mobile
   if (!isMobile) {
     return null;
   }
 
   return (
-    <div className="bottom-bar">
-      <a href="tel:+1234567890" className="cta-button phone-button">
-        <FaPhoneAlt size={18} />
-        <span>Call Now</span>
-      </a>
-      <a href="https://wa.me/1234567890" className="cta-button whatsapp-button">
-        <FaWhatsapp size={18} />
-        <span>WhatsApp Now</span>
-      </a>
-    </div>
+    <>
+      <div className="bottom-bar">
+        <a href="tel:+1234567890" className="cta-button phone-button">
+          <FaPhoneAlt size={18} />
+          <span>Call Now</span>
+        </a>
+        <a href="https://wa.me/1234567890" className="cta-button whatsapp-button">
+          <FaWhatsapp size={18} />
+          <span>WhatsApp Now</span>
+        </a>
+      </div>
+      {/* Sticky Footer */}
+      <div className="sticky-footer">
+        <p>© 2025 SV Banquet Halls | All Rights Reserved</p>
+      </div>
+    </>
   );
 };
 
