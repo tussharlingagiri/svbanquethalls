e8341379a093f4c2d61247ae6443028c7768d782
Vijaya Lingagiri <vlingagiri@gmail.com>
2025-01-12 20:06:24 +0000

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,36 @@ const Footer = () => (
     <div className="footer-content">
       <p>&copy; {new Date().getFullYear()} SV Banquet Halls. All Rights Reserved.</p>
       <ul className="social-links">
-        <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
-        <li><a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-        <li><a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+        <li>
+          <a
+            href="https://www.facebook.com/svbanquethalls"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/svbanquethalls/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/svbanquethalls/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
       </ul>
     </div>
   </footer>
 );
 
 export default Footer;
-
